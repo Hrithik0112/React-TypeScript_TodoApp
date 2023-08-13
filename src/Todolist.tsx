aa519065680d3bfc03b36a5622d967f14d030d09
Hrithik <slumdog4321@gmail.com>
2023-08-14 00:02:35 +0530

    Basic Structure and Styling Added

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Todolist = () => {
-  return <div>Todolist</div>;
+  return (
+    <div className="main_container">
+      <h1>Todo List</h1>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+      </ul>
+      <input type="text" placeholder="Add Your Task"></input>
+      <button>Add</button>
+    </div>
+  );
 };
 
 export default Todolist;
