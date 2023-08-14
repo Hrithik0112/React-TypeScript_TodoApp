1ef24faa24ff649c6311670b874144b6ceb27c0a
Hrithik <slumdog4321@gmail.com>
2023-08-14 07:37:14 +0530

    Functionality Added

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,15 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+
+interface item {
+  id: number;
+  text: string;
+  completed: boolean;
+}
 
 const Todolist = () => {
+  const [todos, setTodos] = useState<item[]>([
+    { id: 1, text: "HAve to Learn About Redis", completed: false },
+    { id: 2, text: "Have To read documentation About TypeScript", completed: false },
+  ]);
+  const [input, setInput] = useState<string>("");
+
+  const handleToggle = (id: number) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed: !todo.completed };
+        }
+        return todo;
+      })
+    );
+  };
+
+  const handleClick = () => {
+    const newTodo: item = { id: Date.now(), text: input, completed: false };
+    setTodos([...todos, newTodo]);
+    setInput("");
+  };
   return (
     <div className="main_container">
       <h1>Todo List</h1>
       <ul>
-        <li>Item 1</li>
-        <li>Item 2</li>
+        {todos.map((todo) => (
+          <li
+            key={todo.id}
+            onClick={() => handleToggle(todo.id)}
+            style={{ textDecoration: todo.completed ? "line-through" : "none" }}
+          >
+            {todo.text}
+          </li>
+        ))}
       </ul>
-      <input type="text" placeholder="Add Your Task"></input>
-      <button>Add</button>
+      <input
+        value={input}
+        type="text"
+        placeholder="Add Your Task"
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={handleClick}>Add</button>
     </div>
   );
 };
